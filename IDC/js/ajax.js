35dcb12205810dac11b67357fed28acc674db109
FZfangzheng <774380232@qq.com>
2018-09-08 19:59:39 +0800

    change

--- IDC/js/ajax.js
+++ IDC/js/ajax.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
   var aj = $.ajax( {    
-    url:'api/info/selectHostInfo.ashx',    
+    url:'api/info/getHostInfoAll.ashx',    
     data:{    
              ip : ip,    
-             username : username,    
-             operation : operation
+             username : username//username怎么来。从cookie里读取，这个cookie是从登陆的地方获取的
     },    
     type:'post',    
-    cache:false,    
+    cache:false,
+    async: false,  
     dataType:'json',    
     success:function(data) {    
-        for(int i = 0; i < data.length; i++) { //循环后台传过来的Json数组
+        //循环后台传过来的Json数组
+        for(int i = 0; i < data.length; i++) { 
 			var datas = data[i];
+            //把数据添加到页面
 			$("#table").children("tbody").append("<tr><th>"+ i +"</th><td>"+data.ip+"</td><td>"+data.username+"</td><td><button onclick="window.location.href='detail.html'">"+data.operation+"</button></td></tr>");
 		}
 
+		
+		
+		
+		
+		
+		
+		
+		
+		//存cookie这里，应该是从js获取点击的列表的某行的对应的ip，然后在detail里获取cookie然后再次请求接口，所以下面都要改
 		//存cookie
 		console.log(data);
          if(data.status =='200'){
@@ -32,8 +43,7 @@ $(document).ready(function(){
             location.href='index.html';
          }
          if(data.status =='404'){
-           
-            //alert(data.error);
+            alert(data.error);
             return false;
          }
      },    
